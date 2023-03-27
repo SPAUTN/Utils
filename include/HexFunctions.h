7149e252fa43946d3874df903acb6bb6c567b186
Julian <julian.camargo@sitrack.com>
2023-03-26 22:22:15 -0300

    hexFunctions added

---
+++ include/HexFunctions.h
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <stdio.h>
+/**
+ * @brief Convert hex to ASCII
+ * 
+ * @param hex 
+ * @return String 
+ */
+String hexToASCII(String);
+/**
+ * @brief Convert ASCII to hex
+ * 
+ * @param ascii 
+ * @return String 
+ */
+String asciiToHex(String ascii);
---
+++ src/HexFunctions.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <stdio.h>
+String hexToASCII(String hex) {
+  String ascii = "";
+  for(int i=0; i<hex.length(); i+=2) {
+    String ch = hex.substring(i, i+2);
+    char c = (char) (int)strtol(ch.c_str(), NULL, HEX);
+    ascii += (char) c;
+  }
+  return ascii;
+}
+
+String asciiToHex(String ascii) {
+  String hex = "";
+  for(int i=0; i<ascii.length(); i++) {
+    char c = ascii.charAt(i);
+    int code = c;
+    String h = String(code, HEX);
+    hex += h;
+  }
+  return hex;
+}
