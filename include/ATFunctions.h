8671628d969ee2fd55d80068718568aa87764735
Julian <julian.camargo@sitrack.com>
2023-03-26 22:35:31 -0300

    at commands as macros

--- include/ATFunctions.h
+++ include/ATFunctions.h
@@ -1,5 +1,16 @@
 #include <Arduino.h> 
 
+// ---------------- Macros for the AT commands ----------------
+#define AT_RESET "ATZ"
+
+// Baud rate configuration
+#define AT_BAUD_38400_CONFIG_SET "AT+BAUD=38400"
+#define AT_BAUD_115200_CONFIG_SET "AT+BAUD=115200"
+
+// P2P configuration
+#define AT_P2P_CONFIG_SET "AT+P2P=915000000:7:0:0:10:14"
+#define AT_P2P_CONFIG_GET "AT+P2P=?"
+#define AT_P2P_CONFIG_TX_SET "AT+PRECV=0" 
 /**
  * @brief Read the serial port 2 and return the response
  * 
